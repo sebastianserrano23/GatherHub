367a5efa39ed4080002d6f7b1e13e4149acc9a9d
Sebastian <serranosebastian23@csu.fullerton.edu>
2023-09-29 19:37:43 -0700

    made a forms.py to make user login/create account

--- gatherhub/core/forms.py
+++ gatherhub/core/forms.py
@@ -1,7 +1,11 @@
+from dataclasses import fields
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    username = forms.CharField(max_length=35, required=True, help_text='user name must be 35 characters or fewer')
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'pasword')
